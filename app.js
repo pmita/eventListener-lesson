7a6cfbb2135cbff966ee9ae578bc6cbf5e7c5c1b
pmita <panos.mita2@outlook.com>
2021-04-30 14:21:52 +0100

    event listeners re-factored

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('.btn');
 const counter = document.querySelector('.counter');
 let counterNumber = parseFloat(counter.innerText);
 
-
+/*
 const decreaseButton = buttons[0];
 const resetButton = buttons[1];
 const increaseButton = buttons[2];
@@ -22,5 +22,27 @@ increaseButton.addEventListener('click', function(){
     counterNumber = counterNumber + 1;
     counter.innerText = counterNumber;
 });
+*/
+
+buttons.forEach( function(button, index){
+    button.addEventListener('click', function(){
+        switch(index){
+            case 0:
+                counterNumber = counterNumber -1;
+                counter.innerText = counterNumber; 
+                break; 
+            case 1:
+                counterNumber = 0;
+                counter.innerText = counterNumber;
+                break;
+            case 2:
+                counterNumber = counterNumber + 1;
+                counter.innerText = counterNumber;
+                break;
+        }
+    });
+});
+
+
 
 
